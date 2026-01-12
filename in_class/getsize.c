6b838b9cc00f3809c0f07c767e792a7e77e3e73d
brookham <bhamilton3264@gmail.com>
2026-01-12 14:10:03 -0800

    fix

--- in_class/getsize.c
+++ in_class/getsize.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]){
     for (int i = 1; i < argc; i++){
       struct stat buf;
       stat(argv[i], &buf);
-      printf("%s: %d",argv[i], buf.st_size);
+      printf("%s: %d\n",argv[i], buf.st_size);
     }
   }
 
