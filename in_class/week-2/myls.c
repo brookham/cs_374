75324d4828477f194a9c4396bd158c81edc5172f
brookham <bhamilton3264@gmail.com>
2026-01-14 12:03:10 -0800

    in class 2.4

---
+++ in_class/week-2/myls.c
@@ -0,0 +1,16 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdio.h>
+
+int main()
+{
+  DIR* dir_point = opendir("/");
+
+  struct dirent *d = readdir(dir_point);
+  printf("%s\n", d->d_name);
+  
+  while (d->d_name != NULL){
+    struct dirent *d = readdir(dir_point);
+    printf("%s\n", d->d_name);
+  }
+}
