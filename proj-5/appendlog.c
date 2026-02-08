3eba91f7b2e938264d1b98070f39008d3ab81c59
brookham <bhamilton3264@gmail.com>
2026-02-09 06:50:23 +0900

    proj-5 almost complete

--- proj-5/appendlog.c
+++ proj-5/appendlog.c
@@ -11,36 +11,53 @@
 
 int main(int argc, char *argv[])
 {
-  int fd = open("data.dat", O_RDWR|O_CREAT|O_TRUNC, 0666);
+  struct flock lock = {
+      .l_type = F_WRLCK,
+      .l_whence = SEEK_SET,
+      .l_start = 0,
+      .l_len = 0
+
+  };
+  int fd = open("data.dat", O_RDWR | O_CREAT | O_TRUNC, 0666);
+
   ftruncate(fd, LENGTH);
 
-  void *data = mmap(NULL, LENGTH, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+  void *data = mmap(NULL, LENGTH, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
   int *header = data;
+  header[0] = 0;
+  header[1] = 0;
 
-  int offset = header[0];
-  int count = header[1];
-
-  for (int i = 1; i < argc; i += 2){
+  for (int i = 1; i + 1 < argc; i += 2)
+  {
 
     pid_t pid = fork();
-    char *prefix = NULL;
 
-    if (pid == 0){
-      int *num = ((char *)data + sizeof(int) * 2 + offset);
-      *num = atoi(argv[i]);
+    if (pid == 0)
+    {
+      lock.l_type = F_WRLCK;
+      fcntl(fd, F_SETLKW, &lock);
+      for (int j = 0; j < atoi(argv[i]); j++)
+      {
 
-      prefix = (char *)(num + 1);
-      strcpy(prefix, argv[i + 1]);
-      offset += 32;
-      count += 1;
+        char *log = (char *)data + sizeof(int) * 2 + header[0];
+        snprintf(log, 32, "%d: %s", header[1], argv[i + 1]);
 
-      header[0] = offset;
-      header[1] = count;
 
-    } else {
-      printf("%d; %s\n",count, prefix);
+        header[0] += 32;
+        header[1] += 1;
+      }
+      lock.l_type = F_UNLCK;
+      fcntl(fd, F_SETLKW, &lock);
+      _exit(0);
+    }
+    else
+    {
+
       wait(NULL);
     }
+    printf("%.*s", header[0], (char *)data + sizeof(int) * 2);
+    munmap(data, LENGTH);
+    close(fd);
   }
 }
